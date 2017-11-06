30dc7fe206ca8d9b1000808a0dd00d72d32c05ad
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-11-06 15:11:58 -0200

    Adicionado a lib para templates

---
+++ application/views/templates/default/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Matrícula On-line BOM JESUS/IELUSC</title>
+    <?php echo $css_head; ?>
+</head>
+<body>
+    <?php echo $content; ?>
+    <?php echo $js_footer; ?>
+</body>
+</html>
