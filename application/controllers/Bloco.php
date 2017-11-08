d43986c2a0e9e20b0916b93c2db297053958ab3c
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-11-08 14:39:17 -0200

    Preparando a view com load dos itens via ajax

--- application/controllers/Bloco.php
+++ application/controllers/Bloco.php
@@ -18,17 +18,34 @@ class Bloco extends CI_Controller
         }
 
         //retorna a view
+        $this->template->show('bloco', 'list');
     }
 
 
     public function list_itens()
     {
-       
-       $this->load->library('response');
+        if ($this->input->is_ajax_request()) {
 
-       $this->response->set_data(array('nome' => 'Lucas', 'idade' => 23, 'fuma' => false, 'score' => array(1,4,6,8,234)));
+            $this->load->model('bloco_select_model', 'bsm');
+            $this->load->library('response');
+
+            $blocos = $this->bsm->fetch_all();
+            $count_blocos = count($blocos);
+            $msg = "Foram encontrados {$count_blocos} blocos";
+
+            if (empty($blocos)) {
+                $msg = "Nenhum bloco foi encontrado";
+            }
+
+            $this->response->set_data($blocos);
+            $this->response->set_valid(true);
+            $this->response->set_msg($msg);
         
-       return $this->response->get_res_json(); 
+            return $this->response->get_res_json(); 
+
+        }
+
+        show_404();
     }
 
     
---
+++ application/views/bloco/list.php
@@ -0,0 +1,11 @@
+<table border="1" width="100%">
+<thead>
+<tr>
+<th width="10%">Id</th>
+<th>Nome</th>
+<th width="30%">#</th>
+</tr>
+</thead>
+<tbody id="table_content">
+</tbody>
+</table>
