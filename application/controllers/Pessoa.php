5b055e212eab6606c5de61b017571fc3ffd27352
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-11-20 10:52:21 -0200

    Refatorar o modelo pessoa e suas dependencias

--- application/controllers/Pessoa.php
+++ application/controllers/Pessoa.php
@@ -4,57 +4,43 @@ class Pessoa extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        //adicionados no config/autoload
-        //$this->load->library('template');
-        //$this->load->helper('form');
-        //$this->load->helper('url');
-
+        $this->load->library('template');
+        $this->load->helper('form');
+        $this->load->helper('url');
     }
 
-
-    //retorna um formulario
     public function index($id = null)
     {
         $data = null;
+        
         if ($this->input->method() === 'post') {
-            $this->load->library('form_validation');
 
-            $this->form_validation->set_rules('nom', 'Nome', 'required|max_length[45]|valid_nome_completo');
-            $this->form_validation->set_rules('usu', 'Usuário', 'required|max_length[45]');
-            $this->form_validation->set_rules('sen', 'Senha', 'required|min_length[8]');
-            $this->form_validation->set_rules('dat_nas', 'Data Nascimento', 'required');
-            $this->form_validation->set_rules('ema', 'Email', 'required|valid_email');
-            $this->form_validation->set_rules('cpf', 'CPF', 'required|valid_cpf ');
-
-            if ($this->form_validation->run()) {
-                //salva os dados
-                $this->load->database();
-                $this->form_validation->set_rules('cpf', 'CPF', 'is_unique[pessoa.cpf]');
-
-                $this->load->model('pessoa_model', 'psm');
-
-                //data bind
-                $data_bind = array(
-                    'nom' => $this->input->post('nom'),
-                    'usu' => $this->input->post('usu'),
-                    'sen' => $this->input->post('sen'),
-                    'dat_nas' => $this->input->post('dat_nas'),
-                    'ema' => $this->input->post('ema'),
-                    'cpf' => $this->input->post('cpf'),
-                );
-
-                if ($this->psm->salvar($data_bind, $id)) {
-                    //redireciona para uma pagina de sucesso
+            $data_bind = array(
+                'id'      => $this->input->post('id'),
+                'nom'     => $this->input->post('nom'),
+                'usu'     => $this->input->post('usu'),
+                'sen'     => $this->input->post('sen'),
+                'dat_nas' => $this->input->post('dat_nas'),
+                'ema'     => $this->input->post('ema'),
+                'cpf'     => $this->input->post('cpf'),
+            );
+            
+            $this->load->model('pessoa_model', 'psm');
+            $this->psm->set_data($data_bind);
+            
+            if ($this->psm->validar()) {
+                if ($this->psm->salvar()) {
                     redirect(base_url('sucesso?cadastro=true'));
                 }
             }
         }
 
-        if(!empty($id)){
-            $this->load->model('Pessoa_select','psl');
+        if (!empty($id)) {
+            $this->load->model('pessoa_select','psl');
             $pessoa = $this->psl->filtrar_id($id);
-            $data = array('pessoa'=>$pessoa);
+            $data = array('pessoa' => $pessoa);
         }
+        
         $this->template->show('pessoa', 'form', $data);
 
     }
